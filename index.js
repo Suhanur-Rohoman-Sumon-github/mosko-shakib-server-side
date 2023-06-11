f8e46c05730d089b715ee77ea728c226ceac6de6
suhanur rohoman sumon <sumonsuhanurrohoman@gmail.com>
2023-06-11 09:52:28 +0600

    post method added

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use(cors())
 
 
-
+// TODO:add env file
 const uri = "mongodb+srv://mosko-shakib:S4MTB4k4zLQrSCrz@cluster0.eepi0pq.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,17 +25,40 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         const classes = client.db('mosko-shakib').collection('popular-corses')
+        const instactors = client.db('mosko-shakib').collection('our-instactor')
+        const carts = client.db('mosko-shakib').collection('carts')
+        // carts post start here
+        app.post('/carts',async(req,res)=>{
+            const body = req.body
+            const result = await carts.insertOne(body)
+            res.send(result)
+        })
+        // carts get oparetion here
+        app.get('/carts',async(req,res)=>{
+            const email = req.query.email
+            const quirey = {email:email}
+            const result = carts.findOne(quirey).toArray()
+            res.send(result)
+        })
 
+        // all classes data here
         app.get('/classes',async(req,res)=>{
             const result = await classes.find().toArray()
             res.send(result)
             
         })
+        // popular classes data here
         app.get('/popular-classes',async(req,res)=>{
             const result = await classes.find().sort({ totalStudents: -1 }).limit(6).toArray()
             res.send(result)
             
         })
+        app.get('/instactors',async(req,res)=>{
+            const result = await instactors.find().toArray()
+            res.send(result)
+            
+        })
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
